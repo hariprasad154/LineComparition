04719c60414eb9ff5cef28c6a173807f92eb8c8e
Hari Prasad G <hariprasas154@gmail.com>
2023-01-13 16:47:01 +0530

    added linecomparition

---
+++ LineComparition.java
@@ -0,0 +1,12 @@
+package LineComparition;
+
+public class LineComparition {
+	static double Length (int x1, int x2,int y1,int y2 ) {
+		return Math.sqrt((x2-x1)^2 + (y2-y2)^2);
+	}
+	public static void main(String[] args) {
+		double l1=(Length(1,6,3,4));
+		double l2=(Length(1,6,3,4));
+
+	}
+}
